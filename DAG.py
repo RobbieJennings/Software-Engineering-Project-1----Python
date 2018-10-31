3df81d0eb047ad8b1d2bc33aa58d127d5ef57b2d
Robbie Jennings <robbie.jennings97@gmail.com>
2018-10-31 14:19:02 +0000

    Removed superfluous code from DAG

--- DAG.py
+++ DAG.py
@@ -5,16 +5,12 @@ from collections import deque
 from collections import OrderedDict
 
 
-def itervalues(d, **kw):
-    return iter(d.values(**kw))
-
-
 class DAG():
     """ Directed acyclic graph implementation. """
 
     def __init__(self):
         """ Construct a new DAG with no nodes or edges. """
-        self.reset_graph()
+        self.graph = OrderedDict()
 
     def add_node(self, node_name, graph=None):
         """ Add a node if it does not exist yet, or error out. """
@@ -38,30 +34,9 @@ class DAG():
             return True
         return False
 
-    def reset_graph(self):
-        """ Restore the graph to an empty state. """
-        self.graph = OrderedDict()
-        return True
-
-    def ind_nodes(self, graph=None):
-        """ Returns a list of all nodes in the graph with no dependencies. """
-        dependent_nodes = set(
-            node for dependents in itervalues(graph) for node in dependents
-        )
-        return [node for node in graph.keys() if node not in dependent_nodes]
-
     def validate(self, graph=None):
-        """ Returns Boolean of whether DAG is valid. """
-        graph = graph if graph is not None else self.graph
-        if len(self.ind_nodes(graph)) == 0:
-            return False
-        if self.topological_sort(graph):
-            return True
-        return False
-
-    def topological_sort(self, graph=None):
-        """ Returns a topological ordering of the DAG.
-        Raises an error if this is not possible (graph is not valid).
+        """ Creates a topological ordering of the DAG.
+        Returns whether or not the graph is valid.
         """
         in_degree = {}
         for u in graph:
@@ -86,7 +61,7 @@ class DAG():
                     queue.appendleft(v)
 
         if len(sorted) == len(graph):
-            return sorted
+            return True
         return False
 
     def get_graph(self):
